1255f44572143e9992d0a44890cd113d1c2270c3
zoefinger <fingz300@newschool.edu>
2024-11-05 04:46:49 +0000

    idek

---
+++ js-fruits/fruits.js
@@ -0,0 +1,28 @@
+function fruitColor(fruit) {
+  if (fruit === "banana") {
+    console.log("yellow");
+  } 
+  else if (fruit === "strawberry") {
+    console.log("red");
+  } 
+  else if (fruit === "lime") {
+    console.log("green");
+  } 
+  else if (fruit === "pineapple") {
+    console.log("yellow");
+  } 
+  else if (fruit === "orange") {
+    console.log("orange");
+  } 
+  else if (fruit === "clementine") {
+    console.log("orange");
+  } 
+  else if (fruit === "pear") {
+    console.log("green");
+  } 
+  else {
+    console.log("fruit not found …");
+  } 
+}
+
+fruitColor("lime");
---
+++ testjs/entry-6.js
@@ -0,0 +1,5 @@
+var images = document.getElementById('im1');
+
+images.addEventListener('click', function(images){
+    images.target.classList.toggle ('second-image')
+})
